75627a4a9450a5a0a7b00209fd3641c41eda19d7
Bunny OOG Box <32637051+bunny-oogbox@users.noreply.github.com>
2017-10-12 21:19:43 +0530

    Added Base Source for OOG Mobile

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# oog-mobile
+# Android OOG Mobile for Odoo
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
 
-Odoo On Go Mobile framework integrated with Odoo
+Android OOG Mobile Framework with integration of Odoo provide you base for creating Odoo android apps quickly.
+
 
---
+++ app/src/main/java/com/oogbox/mobile/LaunchScreen.java
@@ -0,0 +1,15 @@
+package com.oogbox.mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_launch_screen);
+
+        //TODO: Set version number txtAppVersion
+    }
+}
